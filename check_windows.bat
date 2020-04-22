a02cee6a75955aee01d3ec6b3bb9b85f86764e12
Laurent Erignoux <lerignoux@gmail.com>
2020-04-22 23:46:19 +0800

    Adding windows bat draft

---
+++ check_windows.bat
@@ -0,0 +1,2 @@
+powercfg /q 381b4222-f694-41f0-9685-ff5bb260df2e 7516b95f-f776-4464-8c53-06167f40cc99 3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e
+start "" https://laurent.erignoux.fr/computer-waste/index.html?screenCount={}&screenOff={}
